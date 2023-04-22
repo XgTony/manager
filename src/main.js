b0d9860a2c370a3c87abbfbd51f90be187aefa2b
xgTony <374547833@qq.com>
2023-04-22 23:12:42 +0800

    rad

--- src/main.js
+++ src/main.js
@@ -3,22 +3,24 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // 引入element-plus 组件库
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import router from './router'
-import axios from 'axios'
-import config from './config'
+import store from './store'
+import api from '@/api/index.js'
 
-// 获取环境变量
-// 环境变量通常可以从 process.env 获得, 注意 Vite 默认是不加载 .env 文件的
-// const env = import.meta.env
 import request from './utils/request'
 import storage from './utils/storage'
 const app = createApp(App)
 app.use(router)
 app.use(ElementPlus)
+app.use(store)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 // 全局挂载$request
 app.config.globalProperties.$request = request
 // 全局挂载$storage
 app.config.globalProperties.$storage = storage
-
+app.config.globalProperties.$api = api
 app.mount('#app')
