a84599f234c4c7af23bc2dacb6fbb4e482da53a9
xgTony <374547833@qq.com>
2023-04-23 19:57:16 +0800

    feat:后台欢迎页页面搭建

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -4,11 +4,46 @@
 </script>
 
 <template>
-  <h1>welcome</h1>
-  <el-button>11</el-button>
-  <router-link to="/login">去登录</router-link>
+  <div class="welcome">
+    <div class="content">
+      <div class="sub-title">欢迎体验</div>
+      <div class="title">XX通用后台管理系统</div>
+      <div class="desc">—— Vue3.0 Elementplues Node Koa技术栈</div>
+    </div>
+    <div class="img"></div>
+  </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.welcome {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  .content {
+    position: relative;
+    top: -30px;
+    .sub-title {
+      font-size: 30px;
+      line-height: 42px;
+      color: #333;
+    }
+    .title {
+      font-size: 40px;
+      line-height: 62px;
+      color: #409eff;
+    }
+    .desc {
+      text-align: right;
+      font-size: 14px;
+      color: #999;
+    }
+  }
+  .img {
+    margin-left: 130px;
+    background-image: url('./../assets/images/welcome.png');
+    width: 371px;
+    height: 438px;
+  }
+}
 </style>
