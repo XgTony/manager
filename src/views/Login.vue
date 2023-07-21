e8dff499b1cbfa50de66d2096b6473270b7364a1
lironghao <374547833@qq.com>
2023-07-21 18:48:04 +0800

    feat:完善登录页面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,8 +10,8 @@ const userForm = ref(null)
 const router = useRouter()
 const data = reactive({})
 const user = reactive({
-	userName: '',
-	userPwd: '',
+	userName: 'admin',
+	userPwd: '1234',
 })
 const rules = reactive({
 	userName: [
@@ -52,26 +52,42 @@ const login = () => {
 	<div class="login-wrapper">
 		<div class="modal">
 			<el-form ref="userForm" :model="user" status-icon :rules="rules">
-				<div class="title">123</div>
-				<el-form-item prop="userName">
-					<el-input type="text" v-model="user.userName">
-						<template #prefix>
-							<el-icon><User /></el-icon>
-						</template>
-					</el-input>
-				</el-form-item>
-				<el-form-item prop="userPwd">
-					<el-input type="password" v-model="user.userPwd">
-						<template #prefix>
-							<el-icon><View /></el-icon>
-						</template>
-					</el-input>
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" class="btn-login" @click="login"
-						>登录</el-button
-					>
-				</el-form-item>
+				<div class="title">OA后台管理系统</div>
+				<div class="form">
+					<el-form-item prop="userName">
+						<el-input
+							type="text"
+							v-model="user.userName"
+							size="large"
+							disabled
+						>
+							<template #prefix>
+								<el-icon><User /></el-icon>
+							</template>
+						</el-input>
+					</el-form-item>
+					<el-form-item prop="userPwd">
+						<el-input
+							type="password"
+							v-model="user.userPwd"
+							size="large"
+							disabled
+						>
+							<template #prefix>
+								<el-icon><View /></el-icon>
+							</template>
+						</el-input>
+					</el-form-item>
+					<el-form-item>
+						<el-button
+							type="primary"
+							class="btn-login"
+							@click="login"
+							size="large"
+							>登录</el-button
+						>
+					</el-form-item>
+				</div>
 			</el-form>
 		</div>
 	</div>
@@ -82,7 +98,9 @@ const login = () => {
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	background-color: #f9fcff;
+	// background-color: #f9fcff;
+	background: #456;
+	font-family: 'Open Sans', sans-serif;
 	width: 100vw;
 	height: 100vh;
 	.modal {
@@ -91,14 +109,17 @@ const login = () => {
 		background-color: #fff;
 		border-radius: 4px;
 		box-shadow: 0px 0px 10px 3px #c7c9cb4d;
+		text-align: center;
 		.title {
-			font-size: 50px;
+			font-size: 30px;
 			line-height: 1.5;
 			text-align: center;
 			margin-bottom: 30px;
 		}
-		.btn-login {
-			width: 100%;
+		.form {
+			.btn-login {
+				width: 100%;
+			}
 		}
 	}
 }
